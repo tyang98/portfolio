ce65a52568fea31946754a877164daf972d7fabe
Tony Yang <tony.yang98@gmail.com>
2022-08-22 20:50:34 -0400

    new content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { PageLayout, PageTitle } from "../components"
-import { Container, Image } from "react-bootstrap"
-import { Link, graphql } from "gatsby"
-import { SEO } from "../utils"
+import React from "react";
+import { PageLayout, PageTitle } from "../components";
+import { Container, Image } from "react-bootstrap";
+import { Link, graphql } from "gatsby";
+import { SEO } from "../utils";
 
 export default ({ data }) => {
-  const { author, designations, unemployed } = data.site.siteMetadata
+  const { author, designations, unemployed } = data.site.siteMetadata;
 
   return (
     <PageLayout>
@@ -30,7 +30,7 @@ export default ({ data }) => {
             ))}
           </p>
           <p className="i-5 mt-4 pt-2">
-            Hi there, I'm <b>Tony</b>. I am a rising senior at&nbsp;
+            Hi there, I'm <b>Tony</b>. I am a senior at&nbsp;
             <a
               href="https://www.cornell.edu/"
               target="_blank"
@@ -41,9 +41,10 @@ export default ({ data }) => {
             &nbsp;studying Computer Science.
           </p>
           <p className="i-5">
-            I am passionate about designing technology for social impact. Last
-            summer, I worked as a Software Engineer Intern at Capital One on
-            the&nbsp;
+            I am passionate about designing technology for social impact. This
+            past summer I worked as a Software Engineering Intern at Google on
+            the YouTube Music Tools team. Two summers ago, I worked as a
+            Software Engineer Intern at Capital One on the&nbsp;
             <a
               href="https://creditwise.capitalone.com/"
               target="_blank"
@@ -63,10 +64,11 @@ export default ({ data }) => {
               Cornell Design & Tech Initiative
             </a>
             &nbsp;helping solve real problems to assist the community. I have
-            experience with both frontend and backend web (React, Express)
-            development, as well as mobile (Android, Flutter, React Native)
-            development. Also, my interests currently lie in full-stack
-            development, Programming Language Theory, and database systems.
+            experience with both frontend and backend web (React, Express,
+            Polymer, Spanner) development, as well as mobile (Android, Flutter,
+            React Native) development. Also, my interests currently lie in
+            full-stack development, Programming Language Theory, and computer
+            viion.
           </p>
           <p className="i-5">
             Aside from programming and tech, I enjoy music, basketball,
@@ -93,12 +95,9 @@ export default ({ data }) => {
               <p className="unemployed">
                 <small>
                   I am{" "}
-                  <b>
-                    currently looking for Software Engineer 
-                    opportunities.
-                  </b>
-                  ! If you like what you <Link to="/projects">see</Link>, you
-                  can contact me at{" "}
+                  <b>currently looking for New Grad Software Engineer opportunities.</b>!
+                  If you like what you <Link to="/projects">see</Link>, you can
+                  contact me at{" "}
                   <b>
                     <a href="mailto:txy3@cornell.edu">txy3@cornell.edu</a>
                   </b>
